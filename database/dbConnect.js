4f52bde6c3acf3163ec2a5a8efad87020f2eb0e8
Vishnu Manimala <71614068+vishnu-manimala@users.noreply.github.com>
2024-05-29 12:17:46 +0530

    Update dbConnect.js

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -3,7 +3,7 @@ const MongoDB_URL = process.env.MONGODB_URL;
 
 module.exports = async(req, res)=>{
     try{
-        await mongoose.connect(MongoDB_URL,{
+        await mongoose.connect('mongodb+srv://vishnu:ayraonline@ayra.ovkdvo3.mongodb.net/?retryWrites=true&w=majority',{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         }).then(()=>{
