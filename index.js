e0d1a4a7a4dc8deb928f4cf13cd3bb2bfaa99963
Vishnu Manimala <71614068+vishnu-manimala@users.noreply.github.com>
2024-05-29 09:50:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ app.use(function(req, res, next) {
   });
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views/userViews'));
-// app.set("views", "./views/userViews");
+
+ app.set("views", "./views/userViews");
 app.use('/public',express.static(path.join(__dirname, './public')));
 
 // MULTER IMAGE PATH FOR FROND END
